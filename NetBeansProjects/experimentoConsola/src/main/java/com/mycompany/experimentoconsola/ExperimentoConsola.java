3ec413b34bc274113a681c61f285bbb3068f8df5
Marco Forton <marcoforton.dev@gmail.com>
2026-07-25 22:23:50 -0500

    Revert "Revert ""linea 6 y 9 editadas"""
    This reverts commit c93ee1666f1bf40b6fb8a5a5e09c788b6ec8f854.

--- NetBeansProjects/experimentoConsola/src/main/java/com/mycompany/experimentoconsola/ExperimentoConsola.java
+++ NetBeansProjects/experimentoConsola/src/main/java/com/mycompany/experimentoconsola/ExperimentoConsola.java
@@ -3,10 +3,10 @@
  */
 
 package com.mycompany.experimentoconsola;
-
+//linea creada después del commit y despues del push 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Scanner; //libreria para el scanner
 /**
  *
  * @author marco
